695d58fe2cebb0535a79acc1b2bcf78a7d9561f8
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-10-26 22:31:26 +0300

    Finish lab 2

--- lab_2.py
+++ lab_2.py
@@ -2,29 +2,88 @@ from generators.linear_congruential import generate_numbers
 
 
 MIN = 100
-MAX = 100
+MAX = 120
 
 
-def generate_point():
-    r = generate_numbers(MIN, MAX, 1)
-    g = generate_numbers(MIN, MAX, 1)
+def choose_variables():
+    points = []
+
+    print('Enter four figure points')
+
+    for i in range(4):
+        print('Point', i + 1)
+        points.append((int(input('x: ')), int(input('y: '))))
+
+    n = int(input('Enter n (points number): '))
+    k = int(input('Enter k (experiments number): '))
+
+    x_coordinates = [point[0] for point in points]
+    y_coordinates = [point[1] for point in points]
+
+    a = min(x_coordinates)
+    b = max(x_coordinates)
+    c = min(y_coordinates)
+    d = max(y_coordinates)
+
+    return points, a, b, c, d, n, k
+
+
+def generate_point(a, b, c, d):
+    r = generate_numbers(MIN, MAX, 1)[0]
+    g = generate_numbers(MIN, MAX, 1)[0]
+
+    x = (b - a) * r + a
+    y = (d - c) * g + c
+
+    return x, y
+
+
+def belong_to_figure(points, point):
+    result = False
+
+    start_x, start_y = points[0]
+
+    for index in range(1, len(points) + 1):
+        p1x, p1y = points[index]
+
+        try:
+            if ((point[0] - start_x) / (start_x - p1x)) == ((point[1] - start_y) / (start_y - p1y)):
+                return True
+        except ZeroDivisionError:
+            return True
+
+        if ((start_y > point[1]) != (p1y > point[1])) \
+            and (point[0] < (p1x - start_x) * (point[1] - start_y) / (p1y - start_y) + start_x):
+            result = not result
+
+        start_x, start_y = p1x, p1y
+
+    return result
 
 
 if __name__ == '__main__':
-    print('Enter rectangle coordinates ((a, c), (b, c), (a, d), (b, d))')
-    a = int(input('a: '))
-    b = int(input('b: '))
+    figure_points, a, b, c, d, points_number, tries = choose_variables()
+    rectangle_points = [(a, c), (b, c), (a, d), (b, d)]
+    rectangle_square = int(((b - a) ** 2) ** 0.5 * ((d - c) ** 2) ** 0.5)
+    m, n = 0, 0
+    squares = []
 
-    while a > b:
-        b = int(input('b should not exceed a: '))
+    for _ in range(tries):
+        points = []
 
-    c = int(input('c: '))
-    d = int(input('d: '))
+        for _ in range(points_number):
+            point = generate_point(a, b, c, d)
 
-    while c > d:
-        d = int(input('d should not exceed c: '))
+            if belong_to_figure(figure_points, point):
+                m += 1
+            if belong_to_figure(rectangle_points, point):
+                n += 1
 
-    n = int(input('Enter n (points number): '))
+        try:
+            squares.append((m / n) * rectangle_square)
+        except ZeroDivisionError:
+            pass
 
-    square = int(((b - a) ** 2) ** 0.5 * ((d - c) ** 2) ** 0.5)
+    average_square = int(sum(squares) / tries)
 
+    print('\nFigure square:', average_square)
