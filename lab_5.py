9b823f6365d32d4029bf52c4ca8ad6a93579ecab
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-11-18 21:43:48 +0200

    Finish lab 5 and lab 6

--- lab_5.py
+++ lab_5.py
@@ -1,48 +1,67 @@
-C = 100  # equipment
-M = 90  # recall sources
-L = 95  # intensity
-NU = 0.3  # serving_time
-MU = 0.4  # recall_time
+from math import ceil
+from random import uniform
 
 
-if __name__ == '__main__':
-    r = [[] for _ in range(M + 1)]
-    r[M].extend([1, 1])
+def system_with_recall_sources(m, c, mu, nu, la):
+    r = [[] for _ in range(m + 1)]
+    r[m].extend([1, 1])
 
-    for i in range(2, C + 1):
-        r[M].append(((L + (i - 1) * NU + M * MU) * r[M][i - 1] - L * r[M][i - 2]) / (i * NU))
+    for i in range(2, c + 1):
+        r[m].append(((la + (i - 1) * nu + m * mu) * r[m][i - 1] - la * r[m][i - 2]) / (i * nu))
 
-    for j in range(M - 1, -1, -1):
+    for j in range(m - 1, -1, -1):
         b, d = [0], [0]
-        r[j].extend([0 for _ in range(C + 1)])
+        r[j].extend([0 for _ in range(c + 1)])
 
-        for i in range(1, C + 1):
-            b.append(i * NU * (j * MU + b[i - 1]) / (L + j * MU + b[i - 1]))
-            d.append((j + 1) * MU * r[j + 1][i - 1] + (L * d[i - 1]) / (L + j * MU + b[i - 1]))
+        for i in range(1, c + 1):
+            b.append(i * nu * (j * mu + b[i - 1]) / (la + j * mu + b[i - 1]))
+            d.append((j + 1) * mu * r[j + 1][i - 1] + (la * d[i - 1]) / (la + j * mu + b[i - 1]))
 
-        r[j][C] = (j + 1) * MU * sum(r[j + 1][0:C]) / L
+        r[j][c] = (j + 1) * mu * sum(r[j + 1][0:c]) / la
 
-        for i in range(C - 1, -1, -1):
-            r[j][i] = (d[i] + (i + 1) * NU * r[j][i + 1]) / (L + j * MU + b[i])
+        for i in range(c - 1, -1, -1):
+            r[j][i] = (d[i] + (i + 1) * nu * r[j][i + 1]) / (la + j * mu + b[i])
 
     pi0m = 1 / sum(map(sum, r))
     pi = [[ri * pi0m for ri in rj] for rj in r]
-    pi[M][0] = pi0m
+    pi[m][0] = pi0m
 
-    temp = [i * sum(map(lambda pij: pij[i], pi)) for i in range(1, C + 1)]
+    temp = [i * sum(map(lambda pij: pij[i], pi)) for i in range(1, c + 1)]
     average_busy_equipment = sum(temp)
-    print(
-        'Average quantity of busy equipment:',
-        round(average_busy_equipment, 2)
+    average_recall_sources = sum([j * sum(pi[j]) for j in range(1, m + 1)])
+    average_time_from_call_to_service = average_recall_sources / la
+    loss_call_probability = sum(map(lambda pij: pij[c], pi))
+
+    return (
+        average_busy_equipment,
+        average_recall_sources,
+        average_time_from_call_to_service,
+        loss_call_probability
     )
 
-    average_recall_sources = sum([j * sum(pi[j]) for j in range(1, M + 1)])
-    print(
-        'Average quantity of recall sources:',
-        round(average_recall_sources, 2)
-    )
 
-    print('Average time from call to service:', round(average_recall_sources / L, 2))
+if __name__ == '__main__':
+    tests = 10
+    c = 100  # equipment
+    m = 110  # recall sources
+    la = 105  # intensity
 
-    loss_call_probability = sum(map(lambda pij: pij[C], pi))
-    print('Probability of loss initial call', round(loss_call_probability, 2))
+    for test in range(tests):
+        nu = uniform(0, 0.5)  # serving_time
+        mu = uniform(0, 0.5)  # recall_time
+
+        print('--- Test', test + 1, '---')
+        print('Serving time:', round(nu * 60, 2))
+        print('Recall time:', round(mu * 60, 2))
+
+        (
+            average_busy_equipment,
+            average_recall_sources,
+            average_time_from_call_to_service,
+            loss_call_probability
+        ) = system_with_recall_sources(m, c, mu, nu, la)
+
+        print('Average quantity of busy equipment:', ceil(average_busy_equipment))
+        print('Average quantity of recall sources:', ceil(average_recall_sources))
+        print('Average time from call to service:', round(average_time_from_call_to_service * 60, 3))
+        print('Probability of loss initial call:', round(loss_call_probability, 2), '\n')
